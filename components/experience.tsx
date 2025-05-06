0437a5df27ce8d859de8c66deee75039dd8b70b7
onurhan <onurhanhacimustafaoglu@gmail.com>
2025-05-06 15:15:00 +0300

    fix:experince

--- components/experience.tsx
+++ components/experience.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React from "react";
 import SectionHeading from "./section-heading";
 import "react-vertical-timeline-component/style.min.css";
 import {
