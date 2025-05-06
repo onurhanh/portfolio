b9d28bd5435b0145c9d84c18d877b810dfb45856
onurhan <onurhanhacimustafaoglu@gmail.com>
2025-05-06 15:16:34 +0300

    fix:intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Image from "next/image";
-import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
 import { BsArrowRight, BsLinkedin } from "react-icons/bs";
