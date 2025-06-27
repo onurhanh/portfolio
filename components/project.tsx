4c037eb4d2129c5a5306899c808ebb238ddc259b
onurhan <onurhanhacimustafaoglu@gmail.com>
2025-06-27 16:37:40 +0300

    fix:project

--- components/project.tsx
+++ components/project.tsx
@@ -34,7 +34,12 @@ export default function Project({
     >
       <section className="bg-gray-100 max-w-[42rem] border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem] hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20">
         <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
-          <h3 className="text-xl font-semibold">{title}</h3>
+            <div>
+              <Link href={link} target="_blank" rel="noopener noreferrer">
+            <h3 className="text-xl font-semibold">{title}</h3>
+            </Link>
+            </div>
+
           <p className="mt-2 text-[13px] leading-relaxed text-gray-700 dark:text-white/70">
             {description}
           </p>
